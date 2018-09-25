863734c751a979c1b59ad0b34be5e91d6cb749de
Denis <denis@MAC14.local>
2018-09-25 11:33:20 +0700

    Added styled-components

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import './index.scss';
+// import './index.scss';
+
+const Page = styled.div`
+  position: relative;
+  padding: 40px;
+`;
+
+const Background = styled.div`
+  width: 400px;
+  height: 250px;
+  transform: translateZ(0);
+  background: url('../../assets/img/background.jpg');
+  background-size: cover;
+  border-radius: 2px;
+`;
+
+const TitleH2 = styled.h2`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0;
+  color: #2b2b2b;
+`;
 
 export const Banner = () => (
-  <div className="page">
-    <div className="background">
-      <h2>Banner</h2>
-    </div>
-  </div>
+  <Page>
+    <Background>
+      <TitleH2>Banner</TitleH2>
+    </Background>
+  </Page>
 );
